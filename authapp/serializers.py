df34a44268c12ced2cb7603b5df59dda25eeea6c
KosarevPV <pavel999kosarev@gmail.com>
2023-03-22 21:51:32 +0300

    add versions

--- authapp/serializers.py
+++ authapp/serializers.py
@@ -3,8 +3,15 @@ from rest_framework.serializers import ModelSerializer
 from .models import CustomUser
 
 
-class CustomUserModelSerializer(ModelSerializer):
+class CustomUserModelSerializerBasic(ModelSerializer):
 
     class Meta:
         model = CustomUser
         fields = ('username', 'first_name', 'last_name', 'email',)
+
+
+class CustomUserModelSerializer(ModelSerializer):
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'first_name', 'last_name', 'email', 'is_superuser', 'is_staff')
--- todo/settings.py
+++ todo/settings.py
@@ -152,4 +152,5 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.QueryParameterVersioning',
 }
