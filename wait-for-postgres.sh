07cc45021628881455500a4ce82348f384639c96
KosarevPV <pavel999kosarev@gmail.com>
2023-04-02 16:54:54 +0300

    add bach for docker

--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -7,10 +7,7 @@ host="$1"
 shift
 cmd="$@"
 
-until PGPASSWORD="todo" psql -h "$host" -d "todo" -U "drf" -c '\q';>
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
+until PGPASSWORD="todo" psql -h "$host" -d "todo" -U "drf" -c '\q'; do sleep 1; done
 
 >&2 echo "Postgres is up - executing command"
 exec $cmd
